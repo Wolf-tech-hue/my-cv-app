75ef673df728bc3f724f15bb8d9d00a56a952326
Nate <nathanmorake320@gmail.com>
2025-08-29 23:52:07 +0200

    Delete Footer.js

--- Footer.js
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer className="footer">
-      <p>© {new Date().getFullYear()} Nathan Morake | All Rights Reserved</p>
-    </footer>
-  );
-}
-
-export default Footer;
