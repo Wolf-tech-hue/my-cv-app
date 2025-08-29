1e34ac5882ac3eb368fe5bd06c67da2d5efac333
Nate <nathanmorake320@gmail.com>
2025-08-29 23:14:50 +0200

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>© {new Date().getFullYear()} Nathan Morake | All Rights Reserved</p>
+    </footer>
+  );
+}
+
+export default Footer;
